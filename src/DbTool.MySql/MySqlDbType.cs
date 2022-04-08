4aeaffd11c8ad47877c6c729f1f7fa2763edefa1
chenqian <qian.chen3@envision-aesc.com>
2022-04-08 09:45:14 +0800

    添加关联项目。

---
+++ src/DbTool.MySql/MySqlDbType.cs
@@ -0,0 +1,40 @@
+﻿namespace DbTool.DbProvider.MySql
+{
+    internal enum MySqlDbType
+    {
+        BigInt,
+        Binary,
+        Bit,
+        Char,
+        Date,
+        DateTime,
+        DateTime2,
+        DateTimeOffset,
+        Decimal,
+        Float,
+        Image,
+        Int,
+        Money,
+        NChar,
+        NText,
+        Numeric,
+        NVarChar,
+        Real,
+        RowVersion,
+        SmallDateTime,
+        SmallInt,
+        SmallMoney,
+        MediumText,
+        Text,
+        LongText,
+        Time,
+        Timestamp,
+        TinyInt,
+        UniqueIdentifier,
+        VarBinary,
+        VarChar,
+        Variant,
+        Xml,
+        Structured
+    }
+}
